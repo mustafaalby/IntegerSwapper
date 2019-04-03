0216d5741ef545c27bcd1b814850b34ebca65d7f
mustafaalby <albymusty9561@gmail.com>
2019-04-04 00:13:17 +0300

    IntegerSwapper

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntegerSwapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input A Value");
+            Console.WriteLine("A: ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input B Value");
+            Console.WriteLine("B: ");
+            int b =Convert.ToInt32(Console.ReadLine());
+            a=a+b;
+            b=a-b;
+            a=a-b;
+            Console.WriteLine("New value of A: "+a);
+            Console.WriteLine("New value of B: "+b);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
